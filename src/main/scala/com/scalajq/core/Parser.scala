661fbf66e99e8ff71e4319bb71d3201370d0f11d
Guillaume Bouchaud <guillaume.bouchaud@canal-plus.com>
2019-08-08 20:31:38 +0200

    clean code

--- src/main/scala/com/scalajq/core/Parser.scala
+++ src/main/scala/com/scalajq/core/Parser.scala
@@ -6,34 +6,37 @@ import fastparse._
 
 object Parser {
 
-  def numberTerm[_: P]: P[NumberTerm] = num.map(x => NumberTerm(x.toDouble))
-
-  def stringTerm[_: P]: P[StringTerm] = string.map(StringTerm)
-
   def trueTerm[_: P]: P[BooleanTerm]  = `true`.map(_ => BooleanTerm(true))
 
   def falseTerm[_: P]: P[BooleanTerm] = `false`.map(_ => BooleanTerm(false))
 
-  def sliceOrIndexModel[_: P]: P[Option[SliceOrIndexModel]] =
-    P("[" ~ (stringTerm | numberTerm) ~ ":".? ~ numberTerm.? ~ "]" ~ optional).map {
-      case (n1, Some(n2), n3)   => SliceOrIndexModel(n1, Some(n2), n3)
-      case (n1, None, n3)       => SliceOrIndexModel(n1, None, n3)
-    }.?
+  def numberTerm[_: P]: P[NumberTerm] = num.map(x => NumberTerm(x.toDouble))
 
-  def fieldTerm[_: P]: P[SeqTerm] = ((field ~ optional).rep(1) ~ sliceOrIndexModel).rep(1).map { seqField =>
-    val seqTerm: Seq[Term] = seqField.map { s =>
-      wrapTerm(SeqFieldTerm(s._1.map(t => FieldTerm(t._1, t._2))), s._2)
-    }
+  def stringTerm[_: P]: P[StringTerm] = string.map(StringTerm)
 
-    SeqTerm(seqTerm)
+  def sliceOrIndex[_: P]: P[(Term, Option[NumberTerm], Option[String])] =
+    "[" ~ (stringTerm | numberTerm) ~ ":".? ~ numberTerm.? ~ "]" ~ optional
+
+  def sliceOrIndexModel[_: P]: P[Option[SliceOrIndexModel]] = sliceOrIndex.map {
+    case (n1, Some(n2), n3)   => SliceOrIndexModel(n1, Some(n2), n3)
+    case (n1, None, n3)       => SliceOrIndexModel(n1, None, n3)
+  }.?
+
+  def fieldWithSlice[_: P]: P[Seq[(Seq[(String, Option[String])], Option[SliceOrIndexModel])]] =
+    ((field ~ optional).rep(1) ~ sliceOrIndexModel).rep(1)
+
+  def fieldTerm[_: P]: P[SeqTerm] = fieldWithSlice.map { seqField =>
+    SeqTerm(seqField.map(seq =>
+      wrapTerm(SeqFieldTerm(seq._1.map(field => FieldTerm(field._1, field._2))), seq._2)
+    ))
   }
 
   def sliceOrIndexTerm[_: P]: P[Term] = P(Operator.dot ~ sliceOrIndexModel).map(m => wrapTerm(IdentityTerm, m))
 
-  def wrapTerm(t: Term, idx: Option[Model]): Term = idx match {
-    case None => t
-    case Some(SliceOrIndexModel(e1, Some(e2), e3)) => SliceTerm(t, e1, e2, e3)
-    case Some(SliceOrIndexModel(e, None, e3)) => IndexTerm(t, e, e3)
+  def wrapTerm(term: Term, model: Option[Model]): Term = model match {
+    case None => term
+    case Some(SliceOrIndexModel(start, Some(end), optional)) => SliceTerm(term, start, end, optional)
+    case Some(SliceOrIndexModel(start, None, optional)) => IndexTerm(term, start, optional)
   }
 
   def terms[_: P]: P[Seq[Term]] = P(fieldTerm | sliceOrIndexTerm).rep(sep=",")
