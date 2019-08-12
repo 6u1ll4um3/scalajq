eb097e3763101ce81473c4d588630dfaa3bcb7e4
Guillaume Bouchaud <guillaume.bouchaud@canal-plus.com>
2019-08-12 14:06:03 +0200

    Build json object first step

--- src/main/scala/com/scalajq/JQ.scala
+++ src/main/scala/com/scalajq/JQ.scala
@@ -2,20 +2,18 @@ package com.scalajq
 
 import com.scalajq.core.parser.ObjectParser.obj
 import com.scalajq.core.parser.FilterParser.filters
-import com.scalajq.core.{Filters, Object, Output, SeqTerm, Translator}
+import com.scalajq.core.{Object, Output, Translator}
 import fastparse.{P, parse}
 import play.api.libs.json.JsValue
 
 object JQ {
 
-  //private def exprFilter[_: P]: P[Filters] = filters.map(t => Filters(t.map(SeqTerm)))
-  private def exprObject[_: P]: P[Object] = obj.map(Object)
-  private def exprOutput[_: P]: P[Output] = P(exprObject | filters)
-  private def expr: P[_] => P[Output] = exprOutput(_: P[_])
+  private def outputParser[_: P]: P[Output] = P(obj.map(Object) | filters)
+  private def parser: P[_] => P[Output] = outputParser(_: P[_])
 
   def apply(js: JsValue, jq: String): JsValue = {
 
-    val terms: Output = parse(jq, expr).get.value
+    val terms: Output = parse(jq, parser).get.value
     Translator.run(terms, js)
   }
 }
