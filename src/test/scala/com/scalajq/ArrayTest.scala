806ca45acc7b87d42f433b76aa04bbffec78895f
Guillaume Bouchaud <guillaume.bouchaud@canal-plus.com>
2019-08-20 22:13:14 +0200

    test on array parser

--- src/test/scala/com/scalajq/ArrayTest.scala
+++ src/test/scala/com/scalajq/ArrayTest.scala
@@ -1,12 +1,38 @@
 package com.scalajq
 
-import org.scalatest._
+import com.scalajq.core._
+import com.scalajq.core.parser.ArrayParser
+import fastparse.{P, Parsed, parse}
+import org.scalatest.{FlatSpec, MustMatchers}
 import play.api.libs.json._
+
+import scala.collection.mutable.ArrayBuffer
+
 
 class ArrayTest extends FlatSpec with MustMatchers with BaseTest {
 
   val json: JsValue = Json.parse(jsStarWars)
   val jsCharacters = Json.parse(characters.stripMargin)
+
+  "ScalaJq" should "test parser" in {
+
+    val parsed: Parsed[core.Array] = parse("[.name, .place, .author.lastName]", ArrayParser.parser(_: P[_]))
+
+    parsed mustBe Parsed.Success(Array(
+        Filter(
+          ArrayBuffer(
+            SeqTerm(
+              ArrayBuffer(
+                SeqTerm(ArrayBuffer(SeqFieldTerm(ArrayBuffer(FieldTerm("name",None))))),
+                SeqTerm(ArrayBuffer(SeqFieldTerm(ArrayBuffer(FieldTerm("place",None))))),
+                SeqTerm(ArrayBuffer(SeqFieldTerm(ArrayBuffer(FieldTerm("author",None), FieldTerm("lastName",None)))))
+              )
+            )
+          )
+        )
+    ), 33)
+
+  }
 
   "ScalaJq" should "build Array" in {
     JQ(json, "[.name, .place, .author.lastName]") mustBe Json.arr("Star Wars", "in a galaxy far far away", "Lucas")
